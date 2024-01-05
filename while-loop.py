4b1dac4884f2d4243fc315b899404576b6dc9c65
hasbifirasyan <73678968+hasbifirasyan@users.noreply.github.com>
2024-01-05 18:22:31 +0700

    Update while-loop.py
    added exclamation mark

--- while-loop.py
+++ while-loop.py
@@ -6,7 +6,7 @@ isGuessRight = False
 while isGuessRight != True:
     guess = input("Guess a number between 1 and 10: ")
     if int(guess) == number:
-        print("You guessed {}. That is correct! You win!".format(guess))
+        print("You guessed {}. That is correct! You win!!!".format(guess))
         isGuessRight = True
     else:
         print("You guessed {}. Sorry, that isn’t it. Try again.".format(guess))
